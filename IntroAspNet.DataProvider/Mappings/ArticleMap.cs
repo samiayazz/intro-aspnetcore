fe60763f2ad98b7dfa6dc2f912e469ad8e37f66a
samiayazz <ayazzsami@gmail.com>
2023-11-03 20:24:15 +0300

    Add mappings

---
+++ IntroAspNet.DataProvider/Mappings/ArticleMap.cs
@@ -0,0 +1,14 @@
+﻿using IntroAspNet.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IntroAspNet.DataProvider.Mappings
+{
+    public class ArticleMap : IEntityTypeConfiguration<Article>
+    {
+        public void Configure(EntityTypeBuilder<Article> builder)
+        {
+            builder.Property(p => p.Title).HasMaxLength(150);
+        }
+    }
+}
