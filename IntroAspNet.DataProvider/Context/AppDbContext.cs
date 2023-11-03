62b58e180c7fdf5ea7e85f95fda1814eaec62327
samiayazz <ayazzsami@gmail.com>
2023-11-03 17:21:44 +0300

    Add AppDbContext

---
+++ IntroAspNet.DataProvider/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using IntroAspNet.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroAspNet.DataProvider.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected AppDbContext() { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Article> Articles { get; set; }
+    }
+}
