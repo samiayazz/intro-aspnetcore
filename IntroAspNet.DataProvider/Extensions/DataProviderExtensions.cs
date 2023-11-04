d6f8f39bd3d4a267788bb9d4b355e623641ec616
samiayazz <ayazzsami@gmail.com>
2023-11-05 01:57:45 +0300

    Add repository dependency injection with extension

---
+++ IntroAspNet.DataProvider/Extensions/DataProviderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using IntroAspNet.DataProvider.Repositories.Abstraction;
+using IntroAspNet.DataProvider.Repositories.Concretes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IntroAspNet.DataProvider.Extensions
+{
+    public static class DataProviderExtensions
+    {
+        public static IServiceCollection LoadDataProviderExtension(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            return services;
+        }
+    }
+}
